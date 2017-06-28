57ad7620f36230801ccb308ec462ec26f5880c38
Aditya Gupta <adityajdj.gupta@gmail.com>
2017-06-29 01:57:25 +0530

    Add files via upload

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+ require('../../includes/database/connect.db.php');
+ require('../../includes/functions/chat.func.php');
+ 
+ if(isset($_GET['sender']) && !empty($_GET['sender'])){
+	 $sender = $_GET['sender'];
+	  
+	   if(isset($_GET['message']) && !empty($_GET['message'])){
+		  $message = $_GET['message']; 
+		  if(send_msg($sender, $message)){
+			  echo 'message sent';
+		  }else{
+			  echo 'message was not sent';
+		  }
+	   }else{
+		   echo 'no message was entered';
+	   }
+	  
+ }else{
+	 echo 'no name was entered';
+ }
+?>
